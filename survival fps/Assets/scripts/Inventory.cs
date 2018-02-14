06722c0b3045e1f83571542aa4bae1699601bb99
Evan <GIGO1st@gmail.com>
2018-02-13 22:03:46 -0800

    start
    its been started

---
+++ survival fps/Assets/scripts/Inventory.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+
+    public GameObject inventory;
+    public GameObject slotHolder;
+    private bool inventoryEnabled;
+
+    private int slots;
+    private Transform[] slot;
+
+    private GameObject itemPickedUp;
+
+
+    public void Start()
+    {
+        //slots being decteded
+        slots = slotHolder.transform.childCount;
+        slot = new Transform[slots];
+        detectInventorySlots();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if (inventoryEnabled)
+            inventory.SetActive(true);
+        else
+            inventory.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item")
+        {
+            print("colideing");
+            itemPickedUp = other.gameObject;
+            AddItem(itemPickedUp);
+        }
+    }
+
+    public void AddItem(GameObject item)
+    {
+        for(int i= 0; i < slots; i++)
+        {
+            if (slot[i].GetComponent<slot>().empty)
+            {
+                slot[i].GetComponent<slot>().item = itemPickedUp;
+                slot[i].GetComponent<slot>().itemIcon = itemPickedUp.GetComponent<Item>().icon;
+            }
+        }
+    }
+
+    public void detectInventorySlots()
+    {
+        for (int i = 0; i < slots; i++)
+        {
+            slot[1] = slotHolder.transform.GetChild(i);
+        }
+
+    }
+
+}
---
+++ survival fps/Assets/scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //varables
+    public float maxHealth, maxThirst, maxHunger;
+    public float thirstIncreaseRate, hungerIncreaseRate;
+    private float health, thirst, hunger;
+
+    public bool dead;
+
+    //functions
+    public void Start()
+    {
+        health = maxHealth;
+    }
+    public void Update()
+    {
+        if (!dead)
+        {
+            thirst += thirstIncreaseRate * Time.deltaTime;
+            hunger += hungerIncreaseRate * Time.deltaTime;
+        }
+        if (thirst >= maxThirst)
+        {
+            Die();
+        }
+        if (hunger >= maxHunger)
+        {
+            Die();
+        }
+
+    }
+
+
+    public void Die()
+    {
+        dead = true;
+        print("you have died from thirst or hunger");
+    }
+
+
+}
